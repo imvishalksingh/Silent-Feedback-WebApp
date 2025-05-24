a26e597b1d9d3e021059731fb9ac909602bbc560
imvishalksingh <vishal.techengineer@gmail.com>
2025-05-24 16:59:24 +0530

    Again fixed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import '../../App.css';
+import '../App.css';
 import { useNavigate } from 'react-router-dom' ;
 import { v4 as uuidv4 } from 'uuid';
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
-import '../../App.css'
+import { Link } from "react-router-dom";
+import '../App.css';
 
 function Login(){
     return(
